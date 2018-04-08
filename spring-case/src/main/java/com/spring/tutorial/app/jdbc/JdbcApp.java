8af56f25a3f7ed473379c9a2e35f8a1f2b2a1d29
rongbin.xie <rongbin.xie@voyageone.cn>
2018-04-08 13:42:58 +0800

    spring jdbc example based on configuration

--- spring-case/src/main/java/com/spring/tutorial/app/jdbc/JdbcApp.java
+++ spring-case/src/main/java/com/spring/tutorial/app/jdbc/JdbcApp.java
@@ -1,7 +1,9 @@
 package com.spring.tutorial.app.jdbc;
 
+import com.spring.tutorial.configure.SpringConfiguration;
 import com.spring.tutorial.jdbc.StudentJDBCTemplate;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
  * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
@@ -11,13 +13,14 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class JdbcApp {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/conf/jdbc.xml");
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/conf/jdbc.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
         StudentJDBCTemplate studentJDBCTemplate =
                 (StudentJDBCTemplate)context.getBean("studentJDBCTemplate");
         System.out.println("------Records Creation--------" );
-        studentJDBCTemplate.create("Zara", 11);
-        studentJDBCTemplate.create("Nuha", 2);
-        studentJDBCTemplate.create("Ayan", 15);
+        studentJDBCTemplate.create("chris", 11);
+//        studentJDBCTemplate.create("Nuha", 2);
+//        studentJDBCTemplate.create("Ayan", 15);
 
         System.out.println("------Listing Multiple Records--------" );
     }
--- spring-case/src/main/java/com/spring/tutorial/configure/SpringConfiguration.java
+++ spring-case/src/main/java/com/spring/tutorial/configure/SpringConfiguration.java
@@ -1,7 +1,12 @@
 package com.spring.tutorial.configure;
 
+import com.spring.tutorial.jdbc.StudentJDBCTemplate;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 /**
  * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
@@ -12,5 +17,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan("com.spring.tutorial")
 public class SpringConfiguration {
+    @Bean
+    DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/study?useSSL=false");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123456");
+        return dataSource;
+    }
 
+    @Bean
+    StudentJDBCTemplate studentJDBCTemplate(){
+        StudentJDBCTemplate studentJDBCTemplate = new StudentJDBCTemplate();
+        studentJDBCTemplate.setDataSource(dataSource());
+        return studentJDBCTemplate;
+    }
 }
