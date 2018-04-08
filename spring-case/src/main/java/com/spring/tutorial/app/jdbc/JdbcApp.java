82590a7407a4d678c4f4d931a0dc9aee1380b659
rongbin.xie <rongbin.xie@voyageone.cn>
2018-04-08 13:45:01 +0800

    + full example

--- spring-case/src/main/java/com/spring/tutorial/app/jdbc/JdbcApp.java
+++ spring-case/src/main/java/com/spring/tutorial/app/jdbc/JdbcApp.java
@@ -1,9 +1,12 @@
 package com.spring.tutorial.app.jdbc;
 
+import com.spring.tutorial.bean.model.Student;
 import com.spring.tutorial.configure.SpringConfiguration;
 import com.spring.tutorial.jdbc.StudentJDBCTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
 
 /**
  * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
@@ -23,5 +26,21 @@ public class JdbcApp {
 //        studentJDBCTemplate.create("Ayan", 15);
 
         System.out.println("------Listing Multiple Records--------" );
+        List<Student> students = studentJDBCTemplate.listStudents();
+
+        for (Student record : students) {
+            System.out.print("ID : " + record.getId() );
+            System.out.print(", Name : " + record.getName() );
+            System.out.println(", Age : " + record.getAge());
+        }
+
+        System.out.println("----Updating Record with ID = 2 -----" );
+        studentJDBCTemplate.update(2, 20);
+
+        System.out.println("----Listing Record with ID = 2 -----" );
+        Student student = studentJDBCTemplate.getStudent(2);
+        System.out.print("ID : " + student.getId() );
+        System.out.print(", Name : " + student.getName() );
+        System.out.println(", Age : " + student.getAge());
     }
 }
