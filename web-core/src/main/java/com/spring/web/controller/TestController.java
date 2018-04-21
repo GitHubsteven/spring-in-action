b8601f6a3dcde2c0692be01d1d6b22c6da158a18
rongbin.xie <rongbin.xie@voyageone.cn>
2018-04-21 14:33:26 +0800

    关于spring的一些注解的看法在后台暂停一个段落吧，下面是从servlet到spring mvc，从前端到后端的链接部分，虽然暂时中断了java和数据库这部分的研究，但是这只是暂时的，大概往后推迟几天吧

---
+++ web-core/src/main/java/com/spring/web/controller/TestController.java
@@ -0,0 +1,31 @@
+package com.spring.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.portlet.ModelAndView;
+
+/**
+ * @version 1.0.0 COPYRIGHT © 2001 - 2018 VOYAGE ONE GROUP INC. ALL RIGHTS RESERVED.
+ * @Author rongbin.xie
+ * @Description:
+ * @Date: Created at 12:59 2018/4/21.
+ */
+@RequestMapping("/test")
+@Controller
+public class TestController {
+    @RequestMapping("/index")
+    public ModelAndView testIndex(){
+        ModelAndView model = new ModelAndView();
+        model.addObject("message","hello,index");
+        return model;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String printHello(ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+
+}
